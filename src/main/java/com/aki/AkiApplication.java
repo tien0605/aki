e9576354d75e26324bdeab1fd47a1924c1b89277
tientruong0605 <tientruong0605@gmail.com>
2018-02-21 22:23:37 +0700

    First commit

---
+++ src/main/java/com/aki/AkiApplication.java
@@ -0,0 +1,12 @@
+package com.aki;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkiApplication.class, args);
+	}
+}
